e666596d638b08e0f05cd57e0a8c7d3df55d41e4
wd <wd@datacastle.cn>
2017-11-27 14:49:54 +0800

    add beanUtils , add StringUtils.

---
+++ src/main/java/cn/deepmax/util/BeanUtils.java
@@ -0,0 +1,57 @@
+package cn.deepmax.util;
+
+public class BeanUtils {
+
+    private static final String IS_PREFIX = "is";
+    private static final String SET_PREFIX= "set";
+    private static final String GET_PREFIX = "get";
+
+
+    public static String getReadMethodName(String propertyName,String javaType){
+        checkParam(propertyName, javaType);
+        String deCap = normalize(propertyName);
+        if((javaType.contains("boolean")||javaType.contains("Boolean"))){
+            if(propertyName.startsWith(IS_PREFIX)){
+                return propertyName;
+            }else{
+                return IS_PREFIX+deCap;
+            }
+        }else{
+            return GET_PREFIX+deCap;
+        }
+    }
+
+    public static String getWriteMethodName(String propertyName,String javaType){
+        checkParam(propertyName, javaType);
+        String deCap = normalize(propertyName);
+        if((javaType.contains("boolean")||javaType.contains("Boolean")) && propertyName.startsWith(IS_PREFIX)){
+            return SET_PREFIX + propertyName.substring(2);
+        }else{
+            return SET_PREFIX + deCap;
+        }
+    }
+
+
+    private static void checkParam(String propertyName,String javaType){
+        if(StringUtils.isEmpty(propertyName)){
+            throw new IllegalArgumentException("propertyName should not be empty.");
+        }
+        if(StringUtils.isEmpty(javaType)){
+            throw new IllegalArgumentException("JavaType should not be empty.");
+        }
+    }
+
+    private static String normalize(String name){
+        if(StringUtils.isEmpty(name)){
+            return name;
+        }
+        if (name.length() > 1 && Character.isUpperCase(name.charAt(1))){
+            return name;
+        }
+        char chars[] = name.toCharArray();
+        chars[0] = Character.toUpperCase(chars[0]);
+        return new String(chars);
+    }
+
+
+}
--- src/main/java/cn/deepmax/util/StringUtils.java
+++ src/main/java/cn/deepmax/util/StringUtils.java
@@ -1,5 +1,6 @@
 package cn.deepmax.util;
 
+
 public class StringUtils {
 
 
@@ -9,6 +10,72 @@ public class StringUtils {
     public static boolean isNotEmpty(String s){
         return (s!=null && s.length()!=0);
     }
+
+
+    /**
+     *  my_name_space  ->  myNameSpace
+     *  it_is_a_note -> itIsANote
+     * @param source
+     * @return
+     */
+    public static String lowerCaseUnderlineToCamelCase(String source){
+        if(isEmpty(source)){
+            return source;
+        }
+        String[] strings = source.split("_");
+        return toCamelCase(strings);
+    }
+
+    private static String toCamelCase(String[] source){
+        StringBuilder sb = new StringBuilder();
+        int index = 0;
+        for(String it:source){
+            if(isEmpty(it)) continue;
+            if(index==0){
+                sb.append(it);
+            }else{
+                if(it.length()==1){
+                    sb.append(it.toUpperCase());
+                }else{
+                    sb.append(it.substring(0,1).toUpperCase()).append(it.substring(1));
+                }
+            }
+            index++;
+        }
+        return sb.toString();
+    }
+
+    /**
+     *  getUserName -> get_user_name
+     *  getANoteFromWeb -> get_a_note_from_web
+     * @param source
+     * @return
+     */
+    public static String camelCaseToLowerCaseUnderLine(String source){
+        if(isEmpty(source)){
+            return source;
+        }
+        final char UNDERLINE = '_';
+        char[] chars = source.toCharArray();
+        StringBuilder sb = new StringBuilder();
+        for(char it:chars){
+            if(isUpperCase(it)){
+                sb.append(UNDERLINE).append(String.valueOf(it).toLowerCase());
+            }else{
+                sb.append(it);
+            }
+        }
+        return sb.toString();
+    }
+
+    private static boolean isUpperCase(char c){
+
+        return Character.isUpperCase(c);
+
+    }
+
+
+
 
 
 }
