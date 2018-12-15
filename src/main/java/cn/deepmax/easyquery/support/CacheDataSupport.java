f358e4d70fad29567bd9aabd3c26d4f7bb887fe6
woody <woody_me@qq.com>
2018-12-15 12:28:42 +0800

    optimize

--- src/main/java/cn/deepmax/easyquery/support/CacheDataSupport.java
+++ src/main/java/cn/deepmax/easyquery/support/CacheDataSupport.java
@@ -17,23 +17,22 @@ import java.util.function.Function;
  */
 public abstract class CacheDataSupport<ID,D> {
 
-    protected Set<ID> loadedData = new HashSet<>();
-    protected Map<ID, D> cacheData = new ConcurrentHashMap<>(64);
+
+    protected Map<ID, D> cacheData = new ConcurrentHashMap<>(128);
     public static final Logger logger = LoggerFactory.getLogger(CacheDataSupport.class);
 
 
     abstract public D load(ID uniqueKey) throws Exception;
 
     protected <V> V loadThen(ID uniqueKey, Function<D,V> action){
-        if(!loadedData.contains(uniqueKey)){
+        if(!cacheData.containsKey(uniqueKey)){
             synchronized (uniqueKey.toString().intern()){   //? really effect
-                if(!loadedData.contains(uniqueKey)){
+                if(!cacheData.containsKey(uniqueKey)){
                     try{
                         logger.debug("Try to load data "+uniqueKey+" in class ["+this.getClass()+"]");
                         D theData= load(uniqueKey);
                         if(theData!=null){
-                            loadedData.add(uniqueKey);
-                            cacheData.put(uniqueKey, theData);
+                            cacheData.putIfAbsent(uniqueKey, theData);
                             logger.debug("Load data complete , uniqueKey is  ["+uniqueKey+"] in class ["+this.getClass()+"]");
                         }else{
                             throw new EasyQueryException("load(ID target) should always return a non-null instance. ");
