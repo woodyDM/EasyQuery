8cf532e9d03d3619bf66b644d5071ccf400640ef
woody <woody_me@qq.com>
2018-12-20 22:14:58 +0800

    optimize cache support

--- src/main/java/cn/deepmax/easyquery/support/CacheDataSupport.java
+++ src/main/java/cn/deepmax/easyquery/support/CacheDataSupport.java
@@ -18,40 +18,35 @@ import java.util.function.Function;
 public abstract class CacheDataSupport<ID,D> {
 
 
-    protected Map<ID, D> cacheData = new ConcurrentHashMap<>(128);
+    protected Map<ID , D> cacheData = new ConcurrentHashMap<>(128);
     public static final Logger logger = LoggerFactory.getLogger(CacheDataSupport.class);
 
 
     abstract public D load(ID uniqueKey) throws Exception;
 
     protected <V> V loadThen(ID uniqueKey, Function<D,V> action){
-        if(!cacheData.containsKey(uniqueKey)){
-            synchronized (uniqueKey.toString().intern()){   //? really effect
-                if(!cacheData.containsKey(uniqueKey)){
-                    try{
-                        logger.debug("Try to load data "+uniqueKey+" in class ["+this.getClass()+"]");
-                        D theData= load(uniqueKey);
-                        if(theData!=null){
-                            cacheData.putIfAbsent(uniqueKey, theData);
-                            logger.debug("Load data complete , uniqueKey is  ["+uniqueKey+"] in class ["+this.getClass()+"]");
-                        }else{
-                            throw new EasyQueryException("load(ID target) should always return a non-null instance. ");
-                        }
-                    }catch (Exception e){
-                        if(e instanceof EasyQueryException){
-                            throw (EasyQueryException)e;
-                        }else{
-                            throw new EasyQueryException("Fail to load data of type:"+uniqueKey.toString(), e);
-                        }
-                    }
+        cacheData.computeIfAbsent(uniqueKey, (v)->{
+            logger.debug("Try to load data  {}  in class {}", uniqueKey, this.getClass().getName());
+            try{
+                D theData = load(uniqueKey);
+                if(theData==null){
+                    throw new IllegalStateException("Unable to cache null data, the load MUST NOT return null value.");
+                }
+                return theData;
+            }catch (Exception e){
+                if(e instanceof EasyQueryException){
+                    throw (EasyQueryException)e;
+                }else{
+                    throw new EasyQueryException("Fail to load data of type:"+uniqueKey.toString(), e);
                 }
             }
-        }
+        });
         D data = cacheData.get(uniqueKey);
         if(data==null){
-            throw new IllegalStateException("Unable to putIfAbsent data, probably bug. Please contact author.");
+            throw new IllegalStateException("Get null from cache , probably bug. Please contact author.");
         }
         return action.apply(data);
+
     }
 
 
